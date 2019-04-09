e28398a780b43c39185f42fa05908cf19a61377d
Andy Bettisworth <andy@accreu.com>
2019-04-09 14:46:06 -0500

    keydown event handler

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+/**
+ * Change "About us" link to blue.
+ *
+ * Q: Why is it delayed?
+ */
+
 let nav_about_us = document.querySelector("header.main-navigation nav a:nth-child(2)")
-console.log(nav_about_us.getAttribute("href"))
+
 nav_about_us.addEventListener("keydown", (event) => {
-  event.target.setAttribute("href", "https://google.com")
-  console.log(nav_about_us.getAttribute("href"))
+  event.target.style.color = "blue"
 })
