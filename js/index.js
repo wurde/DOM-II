72576929f54d5d7c36e87460aa73b4aeca99cc77
Andy Bettisworth <andy@accreu.com>
2019-04-09 14:24:22 -0500

    npm init -y

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let nav_about_us = document.querySelector("header.main-navigation nav a:nth-child(2)")
+console.log(nav_about_us.getAttribute("href"))
+nav_about_us.addEventListener("keydown", (event) => {
+  event.target.setAttribute("href", "https://google.com")
+  console.log(nav_about_us.getAttribute("href"))
+})
