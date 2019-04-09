9e75d78b6cc543b7a109849f4c090b10fdb5ca75
Andy Bettisworth <andy@accreu.com>
2019-04-09 14:27:41 -0500

    npm install live-server

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// Your code goes here
-
 let nav_about_us = document.querySelector("header.main-navigation nav a:nth-child(2)")
 console.log(nav_about_us.getAttribute("href"))
 nav_about_us.addEventListener("keydown", (event) => {
